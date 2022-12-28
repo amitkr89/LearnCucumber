d290dfabef86adf32374eac7a24207091dadf299
Anit <amit@abc.com>
2022-12-28 23:48:16 +0530

    java coading added

---
+++ src/JavaCoading/AAWelcomeAmit.java
@@ -0,0 +1,7 @@
+package JavaCoading;
+
+public class AAWelcomeAmit {
+	public static void main(String args[]){
+		System.out.println("Welcome Amit");  
+	}  
+}  
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author amit
+ *
+ */
+module LearnCucumber {
+}
